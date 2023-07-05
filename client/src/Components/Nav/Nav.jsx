ec99266da0bc990e2e8ac73c41c78563e606ab59
Andy Sen <andy.sen@icloud.com>
2023-07-05 10:05:08 -0400

    nav menu closes on outside click

--- client/src/Components/Nav/Nav.jsx
+++ client/src/Components/Nav/Nav.jsx
@@ -1,16 +1,24 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Menu from './Menu/Menu';
 import './nav.css';
 
 function Nav() {
     const [ isOpen, setOpen ] = useState(false);
+    const menu = useRef();
+    useEffect(() => {
+        function closeMenu(e) {
+            if (isOpen && menu.current && !menu.current.contains(e.target)) {
+                setOpen(false);
+            }
+        }
+
+        document.addEventListener("mouseup", closeMenu);
+
+        return () => {
+            document.removeEventListener("mouseup", closeMenu);
+        }
+    }, [ isOpen ]);
     
-    
-    function closeMenu(e) {
-        console.log(e.target);
-        if (!isOpen) return;
-        setOpen(false);
-    }
     
     function clickHandler() {
         setOpen(!isOpen);
@@ -18,8 +26,8 @@ function Nav() {
     
     return (
         <nav>
-            <div id="nav-wrap" className="max-width">
-                { isOpen ? <Menu onClick={clickHandler} /> : <></>}
+            <div ref={menu} id="nav-wrap" className="max-width">
+                { isOpen ? <Menu /> : <></> }
                 <button className="icon anim-hover-sm" onClick={clickHandler}><i className="fa-solid fa-bars fa-2xl"></i></button>
                 <img className="icon anim-hover-sm" src="./logos/logo.svg" alt="logo" />
                 <button className="icon anim-hover-sm"><i className="fa-solid fa-magnifying-glass fa-2xl"></i></button>
