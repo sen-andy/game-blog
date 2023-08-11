1b6e15e6b2c4d1f59cb6851b80a04404abcfd10b
Andy Sen <andy.sen@icloud.com>
2023-08-11 10:57:52 -0400

    cors to allow *

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -1,5 +1,5 @@
 module.exports = (req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.header(
       "Access-Control-Allow-Headers",
