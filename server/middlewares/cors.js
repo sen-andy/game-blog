2c4f6cbe8e8f85ec6f5fbc30d6e27561534fb678
Andy Sen <andy.sen@icloud.com>
2023-08-14 09:53:07 -0400

    cors update

--- server/middlewares/cors.js
+++ server/middlewares/cors.js
@@ -1,9 +1,9 @@
 module.exports = (req, res, next) => {
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Credentials", "true");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-    );
-    next();
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
 }
